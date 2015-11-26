d84c3136009c36c70a5ac72ec18a1c7f842415c6
BilalTButt <bilalbutt.614@gmail.com>
2015-11-26 15:13:20 +0100

    changes made

--- ITA2_Website/PhpProject4/ITA2_ISDNWebsite/index.php
+++ ITA2_Website/PhpProject4/ITA2_ISDNWebsite/index.php
@@ -11,13 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-<<<<<<< HEAD:ITA2_Website/ITA2_ISDNWebsite/index.php
-        //echo '<a href="ITA2_Files/index.php">Orginal ITA1 website</a>';
-        echo '<a href = "ITA1_Backup/idex.html">My ITA website</a>'
-=======
-        echo '<a href="ITA2_Files/index.php">Orginal ITA1 website</a>';
-        echo 'Hello Thanh';
->>>>>>> 858e23e10aff525f5a5dde3f68b49962e1d94dec:ITA2_Website/PhpProject4/ITA2_ISDNWebsite/index.php
+        echo '<a href = "../ITA1_Backup/idex.html">My ITA website</a>'
        ?>
     </body>
 </html>
