a0bf9eb797e3d0b8be1991dc4fafa1bcb109b514
BilalTButt <bilalbutt.614@gmail.com>
2015-11-26 19:20:56 +0100

    SignUp form

---
+++ ITA2_WebSite/signUp.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>
+            <link href="style/style.css" type="text/css" rel="stylesheet"/>
+        </title>
+    </head>
+    <body>
+        <div id="FormContainer">
+            <div id="FormLeftDiv">
+        <form id="" action="example" method="post" >
+            <fieldset name="SignUpForm">
+                <div id="FormLeftDiv">
+                    First Name:<br>
+                    <input type="text" name="firsName"><br>
+                    Last Name: <br>
+                    <input type="text" name="lastName"><br>
+                    E-Mail:<br>
+                    <input type="text" name="email"><br><br>
+                    Password:<br>
+                    <input text="password" name="passWord"><br>
+                    Confirm Password:<br>
+                    <input text="password" name="confirmPassword"><br>
+                </div>
+                <div id="FormRightDiv">
+                    <input id="formSubmitBtn" type="submit" value="Submit">
+                    <input id="forResetBtn" type="reset" value="Reset">                    
+                </div>
+            </fieldset>
+        </form>
+        
+        </div>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
