67f7aef74fb58be1a9e0f00ad4d9448193ee2939
thanhhnk <thanhhnk@gmail.com>
2015-11-26 16:31:07 +0100

    Restructure project

--- ITA2_WebSite/script/script.js
+++ ITA2_WebSite/script/script.js
@@ -1,33 +1,33 @@
 $(document).ready(function(){
-    $("#container").load("home/home.php");
+    $("#container").load("webpage/home/home.php");
     $("a.home").click(function () {
-        $("#container").load("home/home.php");});
+        $("#container").load("webpage/home/home.php");});
     $("a.exploreNL").click(function () {
-        $("#container").load("explore/explorenl.php");    });
+        $("#container").load("webpage/explore/explorenl.php");    });
     $("a.culture").click(function () {
-        $("#container").load("explore/culture.php");    });    
+        $("#container").load("webpage/explore/culture.php");    });    
     $("a.transport").click(function () {
-        $("#container").load("explore/publictransport.php");    });    
+        $("#container").load("webpage/explore/publictransport.php");    });    
     $("a.weather").click(function () {
-        $("#container").load("explore/weather.php");    });    
+        $("#container").load("webpage/explore/weather.php");    });    
     $("a.thingstodo").click(function () {
-        $("#container").load("thingstodo/thingstodo.php");	}); 
+        $("#container").load("webpage/thingstodo/thingstodo.php");	}); 
     $("a.food").click(function () {
-        $("#container").load("thingstodo/food.php");	});
+        $("#container").load("webpage/thingstodo/food.php");	});
     $("a.shopping").click(function () {
-        $("#container").load("thingstodo/shopping.php");	}); 
+        $("#container").load("webpage/thingstodo/shopping.php");	}); 
     $("a.cycling").click(function () {
-        $("#container").load("thingstodo/cycling.php");	 }); 
+        $("#container").load("webpage/thingstodo/cycling.php");	 }); 
     $("a.event").click(function () {
-        $("#container").load("events/events.php");	});
+        $("#container").load("webpage/events/events.php");	});
     $("a.summerEvent").click(function () {
-        $("#container").load("events/summerEvents.php");	});
+        $("#container").load("webpage/events/summerEvents.php");	});
     $("a.autumnEvent").click(function () {
-        $("#container").load("events/autumnEvents.php");	});
+        $("#container").load("webpage/events/autumnEvents.php");	});
     $("a.springEvent").click(function () {
-        $("#container").load("events/springEvents.php");	});
+        $("#container").load("webpage/events/springEvents.php");	});
     $("a.winterEvent").click(function () {
-        $("#container").load("events/winterEvents.php");	});
+        $("#container").load("webpage/events/winterEvents.php");	});
     $("a.contact").click(function () {
-        $("#container").load("contact/contact.php");	});
+        $("#container").load("webpage/contact/contact.php");	});
        });
