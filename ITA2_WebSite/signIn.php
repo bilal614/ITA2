27afbbb540e749640156c733f728912d75099619
thanhhnk <thanhhnk@gmail.com>
2015-11-27 21:02:06 +0100

    Finished log in form

---
+++ ITA2_WebSite/signIn.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>ISDN website</title>
+            <meta name="homepage" content="An interactive guide for events in NL for students.">
+            <link href="style/style.css" type="text/css" rel="stylesheet"/>
+            <script src = "jquery/jquery-2.1.4.min.js" type="text/javascript"/></script>
+            <script src="jquery/jquery-2.1.4.js" type="text/javascript"></script>
+<!--            <script type="text/javascript" src="script/script.js">
+            </script> -->
+        </head>
+    <body>
+        <div id="mainContainer">
+            <header>
+                <div id="leftLogo">
+                    <a href="index.php"><img id="logo" src="images/index/logo.png" alt="ISNL"></a>
+                </div>
+                <nav id = "mainMenu">
+                     <ul>
+                       <li>
+                           <a class="exploreNL" href="#Explore NL">Explore NL</a>
+                            <ul>
+                                <li ><a class="culture" href="#Culture">Culture</a></li>
+                                <li ><a class="transport" href="#Public transport">Public transport</a></li>
+                                <li ><a class="weather" href="#Weather">Weather</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                           <a class="thingstodo" href="#Things to do">Things to do</a>
+                            <ul>
+                                <li ><a class="food" href="#Food">Food</a></li>
+                                <li ><a class="shopping" href="#Shopping">Shopping</a></li>
+                                <li ><a class="cycling" href="#Cycling">Cycling</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                           <a class="event" href="#Events">Events</a>
+                            <ul>
+                                <li ><a class="summerEvent" href="#Summer Event">Summer events</a></li>
+                                <li ><a class="autumnEvent" href="#Autumn Event">Autumn events</a></li>
+                                <li ><a class="winterEvent" href="#Winter Event">Winter events</a></li>
+                                <li ><a class="springEvent" href="#Spring Event">Spring events</a></li>
+                            </ul>
+                        </li>
+                        <li ><a class="contact" href="#Contact">Contact</a></li>
+                    </ul>
+                </nav>  
+                <div id="Login">
+                    <div>
+                         <a id="btnLogin">Login</a>
+                         <a id="btnSignUp">Sign up</a>
+                    </div>
+                </div>
+            </header>  
+        <div id="container">
+            <div class="loginMain">
+                <div class="login-form">
+                    <h1>Login</h1>
+                    <div class="head">
+                        <img alt="" src="images/index/user.png">
+                    </div>
+                    <form>
+                        <input type="text" onblur="if (this.value == '') {this.value = 'Email';}" onfocus="this.value = '';" value="Emao;" class="text">
+                        <input type="password" onblur="if (this.value == '') {this.value = 'Password';}" onfocus="this.value = '';" value="Password">
+                        <div class="submit">
+                        <input type="submit" value="LOGIN" onclick="">
+                    </div>	
+                        <p><a href="#">Forgot Password ?</a></p>
+                    </form>
+                </div>
+            </div>
+        </div><!--end div#Container!-->
+        <footer id = "footer">
+            <div class="fLeftcol">
+                <a href="http://www.holland.com/">Holland.com</a>
+                <a href="http://www.iamsterdam.com/en/">I love Amsterdam</a>
+            </div>
+            <div class="fLeftcol">
+                <a href="http://www.roughguides.com/">Rough Guide</a>
+                <a href="https://www.studyinholland.nl/">Study in Holland</a>
+            </div>
+            <div id="fRightcol">
+                <div>Connect with ISNL</div>
+                <nav>
+                <a id ="facebook"></a>
+                <a id ="twitter"></a>
+                </nav>
+            </div>
+        </footer>
+    </div><!--end div#mainContainer!-->
+        
+    </body>
+</html>
