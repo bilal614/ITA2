8822f097a4aa2155b030413708c2c009aa831ae5
BilalTButt <bilalbutt.614@gmail.com>
2015-12-10 14:30:52 +0100

    no changes

--- ITA2_WebSite/model/DBConnection.php
+++ ITA2_WebSite/model/DBConnection.php
@@ -1,5 +1,7 @@
 <?php
 
-mysqli_connect('localhost', 'root', '', '', $port, $socket)
+mysqli_connect('localhost', 'root', '', '', $port, $socket);
+
+$dbObject= new PDO('localhost', 'root', '') 
 ?>
 
