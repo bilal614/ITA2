10324a5475aafb352eeb163228fe8004ba2fdb1c
thanhhnk <thanhhnk@gmail.com>
2016-01-14 22:48:39 +0100

    Added connection for athena server

--- ITA2_WebSite/config/config.php
+++ ITA2_WebSite/config/config.php
@@ -1,8 +1,12 @@
 <?php
-
+//Localhost
 define("DB_DSN","mysql:host=localhost;dbname=ita2");
 define("DB_USERNAME","root");
 define("DB_PASSWORD","");
+//Athena server
+//define("DB_DSN","mysql:host=athena01.fhict.local;dbname=dbi333151");
+//define("DB_USERNAME","dbi333151");
+//define("DB_PASSWORD","rTnrm7Fajk");
 define("PAGE_SIZE",6);
 define("TBL_USER","itausers");
 define("TBL_EVENT","events");
